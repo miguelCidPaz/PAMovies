1252b7209545f5bfeacc2cf7d194f4c415a6c69a
miguelCidPaz <miguel.cid.paz@gmail.com>
2021-12-31 16:43:18 +0100

    arreglados bugs de despliegue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import FilmDetails from "./views/FilmDetails";
 import MainView from './views/MainView'
+import Header from './components/header/header'
 import "./styles.scss";
 
 function App() {
   return (
     <main>
       <Header />
-      <MainView/>
+      <MainView />
     </main>
   );
 }
