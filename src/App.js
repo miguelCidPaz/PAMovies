ef89aca7603c54d46569ba79886159bb842676f2
aldo <aldofcastellanop@gmail.com>
2022-01-01 20:35:36 +0100

    create Divisor component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from "./components/header/header";
 // import FilmDetails from "./views/FilmDetails";
 import MainView from './views/MainView'
-import Header from './components/header/header'
+
 import "./styles.scss";
 
 function App() {
