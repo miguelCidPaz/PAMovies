cdfe2a15ccaf0cbc5e4f25f5c8a94e78fea52fe5
miguelCidPaz <miguel.cid.paz@gmail.com>
2022-01-04 10:24:48 +0100

    Funcionalidad principal con API

--- src/components/FilmDetails/CastComponent.jsx
+++ src/components/FilmDetails/CastComponent.jsx
@@ -1,12 +1,14 @@
 
 const CastComponent = (props) => {
+    const urlForImages = "https://image.tmdb.org/t/p/w500/";
+    const logoDefault = "https://st4.depositphotos.com/14343834/39242/v/450/depositphotos_392426376-stock-illustration-vector-finance-illustration-logo-default.jpg";
     return (
         <div className="castcomponent--container">
             <div className="castcomponent--frame-photo">
-                <img src={props.cast.photo} alt={`Foto de ${props.cast.nameActor}`} />
+                <img src={props.cast.logo_path == null ? logoDefault : urlForImages + props.cast.logo_path} alt={`Foto de ${props.cast.name}`} />
             </div>
             <div className="castcomponent--container-name">
-                <p>{props.cast.nameActor}</p>
+                <p>{props.cast.name}</p>
             </div>
         </div>
     )
--- src/views/FilmDetails.jsx
+++ src/views/FilmDetails.jsx
@@ -4,43 +4,61 @@ import { filmDetail } from '../components/FilmDetails/Data';
 import DetailPresentation from '../components/FilmDetails/DetailPresentation';
 import DetailValorations from '../components/FilmDetails/DetailValorations';
 import DetailTrailer from '../components/FilmDetails/DetailTrailer';
-//Prueba
-
-const idProp = 1;
-const ApiKey = "07e793aeac523d9f4455050b060257c7";
+//import Button from '@mui/material/Button';
 
 const FilmDetails = () => {
+    const [search, setSearch] = useState("movie") //Controlara el tipo de busqueda que se realiza
     const [rating, setRating] = useState(0);
     const [view, setView] = useState(false);
+    const [movie, setMovie] = useState(filmDetail); //Pasara a ser o bien una llamada a la Api o el objeto que reciba por prop
+
+    //Url necesaria para las imagenes
+    const urlForImages = "https://image.tmdb.org/t/p/w500/";
+
+    //Llamada a la api
+    const requestApi = async () => {
+        const idProp = "343611";
+        const ApiKey = "07e793aeac523d9f4455050b060257c7";
+        const URL = `https://api.themoviedb.org/3/movie/${idProp}?api_key=${ApiKey}&language=en-UStr}`;
+        await fetch(URL).then(res => res.json()).then(data => {
+            setMovie(data);
+        })
+    }
+
+    useEffect(() => {
+        requestApi();
+    }, [])
 
     const selectScore = (value) => {
-        setRating(value)
+        setRating(value);
     }
 
     const selectView = () => {
         setView(!view);
     }
 
-    const CastFilm = Object.values(filmDetail.cast);
+    //Controlando renders innecesarios
+    console.log("Renderizado en details")
 
     return (
+
         <section className="filmdetails--main-container">
 
-            <DetailPresentation photo={filmDetail.photo} title={filmDetail.title} />
+            <DetailPresentation photo={urlForImages + movie.poster_path} title={movie.title} />
 
             <DetailValorations view={view} puntuation={rating} rating={filmDetail.rating} selectScore={selectScore} selectView={selectView} />
 
             <section className='filmdetails--section filmdetails--description'>
-                <p>{filmDetail.description}</p>
+                <p>{movie.overview}</p>
             </section>
 
             <section className='filmdetails--section filmdetails--cast'>
-                {CastFilm.map((element, index) => {
+                {movie.production_companies !== undefined ? movie.production_companies.map((element, index) => {
                     return <CastComponent key={index} cast={element} />
-                })}
+                }) : null}
             </section>
+            {movie.video ? <DetailTrailer url={movie.video} /> : null}
 
-            <DetailTrailer url={filmDetail.trailer.url} />
 
         </section>
     )
