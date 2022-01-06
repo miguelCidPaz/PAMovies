185b79e2b194f74289b7fc1147c2709097a72d74
patriglezperez <patriciaggonzalezperez@hotmail.com>
2022-01-06 18:43:35 +0100

    main branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import MainView from "./views/MainView";
 import Header from './components/header/header'
 import "./styles.scss";
-import Header from './components/header/header'
+
 
 function App() {
   return (
