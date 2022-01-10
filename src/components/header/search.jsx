91be99760be1b957a5f16d3561964350d0c67ab3
aldo <aldofcastellanop@gmail.com>
2022-01-10 19:29:56 +0100

    fix problem with return null on listViews and fix on component search finaly

--- src/components/header/search.jsx
+++ src/components/header/search.jsx
@@ -5,48 +5,53 @@ import { TextField } from "@material-ui/core";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import { useNavigate } from "react-router-dom";
 
-export default function ComboBox() {
+export default function ComboBox(props) {
   const [input, setInput] = useState([]);
   const [data, setData] = useState([]);
+  const [value, setValue] = useState("");
+
   const navigate = useNavigate();
 
   const getSelectValue = (e, value) => {
-    if (e.keyCode === 13) {
-      setData([e.target.value]);
-      // console.log(e.target.value);
-    }
+    setData([value]);
   };
+
   useEffect(() => {
-    if (typeof data[0] === "string") {
+    if (typeof data[0] === "string" && data[0] != null) {
       navigate("/listSearch", { state: input });
       console.log(input);
-    } else if (typeof data[0] === "object") {
-      // navigate("/listSearch");
+    } else if (typeof data[0] === "object" && data[0] != null) {
+      navigate("/listSearch", { state: data });
       console.log(data);
     }
   }, [data]);
-
+  let valueInput = value;
   async function getValue(e) {
     let keyWord = e.target.value;
+
     if (/[a-zA-ZñÑ0-9]/.test(keyWord)) {
       try {
         let getData = await axios.get(
           `https://api.themoviedb.org/3/search/movie?api_key=fb1999e69926d1387eb44c3abee6e7c5&language=en-US&query=${keyWord}&include_adult=false`
         );
         setInput(getData.data.results);
-        // console.log(getData);
       } catch (err) {
         console.log(err);
       }
     }
+
+    setValue(valueInput);
   }
   return (
     <>
       <Autocomplete
+        key={true}
         className="search"
-        onKeyDown={getSelectValue}
+        onChange={getSelectValue}
         freeSolo
+        value={value}
         id="free-solo-demo"
+        clearOnBlur
         options={input}
         getOptionLabel={(option) =>
           typeof option.title === "string" || option.title instanceof String
@@ -57,17 +62,13 @@ export default function ComboBox() {
         renderInput={(params) => (
           <TextField
             onChange={getValue}
+            value=""
             className="input-search"
             {...params}
             placeholder="Movie"
           />
         )}
       />
-      {/* <div>
-        {input.map((element) => {
-          return <p key={element.id}>{element.title}</p>;
-        })}
-      </div> */}
     </>
   );
 }
--- src/components/listSearch/listSearch.jsx
+++ src/components/listSearch/listSearch.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Divisor from "../Divisor/Divisor";
 
 function ListSearch() {
+  const [getMovie, setGetMovie] = useState([]);
   const location = useLocation().state;
 
   let link = "https://image.tmdb.org/t/p/w500";
-  console.log(location[0]);
+
+  useEffect(() => {
+    // console.log(getMovie);
+    // location('./detail',{ state :getMovie )
+  }, [getMovie]);
 
   return (
     <div className="container">
       <Divisor title="TITULOS"></Divisor>
       <div className="movie-list">
         {location.map((element) => (
-          <div className="container-movie-list" key={element.id}>
+          <div
+            className="container-movie-list"
+            onClick={() =>
+              setGetMovie({ id: element.id, title: element.title })
+            }
+            key={element.id}
+          >
             <img
               className="img-movie-list"
               src={`${link}${element.poster_path}`}
@@ -21,7 +32,7 @@ function ListSearch() {
             ></img>
             <div className="movie-list-detail">
               <h1>{element.title}</h1>
-              <p>{element.release_date}</p>
+              <p>( {element.release_date} )</p>
             </div>
           </div>
         ))}
