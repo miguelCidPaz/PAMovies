88cad367ff67669781f21d24371580690bba19d7
patriglezperez <patriciaggonzalezperez@hotmail.com>
2022-01-14 21:38:31 +0100

    change arrows

--- src/components/Main/NewFilms.jsx
+++ src/components/Main/NewFilms.jsx
@@ -1,17 +1,34 @@
 import React from "react";
 import Slider from "react-slick";
 import Film from "./Film";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+
+export function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+
+  return (
+    <ArrowForwardIosIcon className={className} style={{...style, background: "red", color:"white", height:"50px" }} onClick={onClick}/>   
+  );
+}
+
+export function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <ArrowBackIosIcon className={className} style={{...style, background: "red", color:"white", height:"50px" }} onClick={onClick}/>   
+  );
+}
 
 export default function NewFilms(props) {
   let settings = {
     dots: true,
-    // infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
-    // centerMode: true,
     arrows: true,
     focusOnSelect: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
     className: "news-slider",
     rows: 2,
     responsive: [
