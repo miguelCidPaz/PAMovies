c027b7a79e0b0513d36464f184b2bb87e5b592a1
aldo <aldofcastellanop@gmail.com>
2022-01-15 19:55:50 +0100

    try catch news component

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -2,22 +2,23 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 
 export default function Prueba() {
-  const [data, setData] = useState();
+  const [data, setData] = useState([""]);
   async function axio() {
-    const getData = await axios.get(
-      `https://newsapi.org/v2/everything?popularity&q=movie&language=en&from=2022-01-13&to=2022-01-15&sortBy=publishedAt&apiKey=8a57f5f9c91040eda543c4c779c1512c`
-    );
-    setData(getData.data);
+    try {
+      const getData = await axios.get(
+        `https://newsapi.org/v2/everything?popularity&q=movie&language=en&from=2022-01-13&to=2022-01-15&sortBy=publishedAt&apiKey=8a57f5f9c91040eda543c4c779c1512c`
+      );
+      setData(getData.data);
+    } catch (error) {
+      console.log(error);
+    }
   }
-  useEffect(() => {
-    data.articles.map((element) => console.log(element.content));
-  }, [data]);
 
   return (
     <>
       <button onClick={axio}>try</button>
-      {data.articles.map((element) => {
-        return <p>{element.content.slice(0, 200)}</p>;
+      {data.articles?.map((element, key) => {
+        return <p key={key}>{element.content.slice(0, 200)}</p>;
       })}
     </>
   );
