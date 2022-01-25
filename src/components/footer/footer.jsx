237bea2d63a09369d2c93c271e4fe291ca3b0acf
aldo-castellano <92597205+aldo-castellano@users.noreply.github.com>
2022-01-25 18:06:53 +0100

    create footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="container-footer">
+          <p>categorias</p>
+          <p>sobre nosotros</p>
+        </div>
+      </div>
+      <div className="subfooter">
+        <p>
+          Desarrollada por PAM <span>todos los derechos reservados</span>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
